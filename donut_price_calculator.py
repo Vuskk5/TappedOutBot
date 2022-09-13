c536255f13c899d2aa090ae8d54ab48e82536b27
kfirdoron <kfir.doron@perimeter81.com>
2022-09-13 11:09:59 +0300

    updated calculator functions

--- donut_price_calculator.py
+++ donut_price_calculator.py
@@ -1,12 +1,12 @@
 from typing import Union
 
 
-def calculate_donut_price(multiplier: float, price: int, exp: int) -> float:
+def calculate_donut_price(multiplier: float, price: int, exp: int, **kwargs) -> float:
     exp_per_purchase = exp * 5 * ((multiplier + 100) / 100)
     return 1_000_000 / exp_per_purchase * (price * 0.75 / 3) + 16666
 
 
-def donuts_per_item(multiplier: float, price: Union[int, list[int]], exp: int) -> float:
+def donuts_per_item(multiplier: float, exp: int, **kwargs) -> float:
     exp_per_purchase = exp * 5 * ((multiplier + 100) / 100)
     return exp_per_purchase / 1_000_000 * 3
 
@@ -17,24 +17,26 @@ def main():
             'price': 132000,
             'exp': 13200
         },
-        'KEM': {
+        'Kwik-E-Mart': {
             'price': 14400,
             'exp': 2000
         }
     }
 
-    multiplier = 1617.52
+    multiplier = 2060.52
 
-    price_for_bloodmobiles = calculate_donut_price(multiplier=multiplier, **items['Bloodmobile'])
-    price_for_kem = calculate_donut_price(multiplier=multiplier, **items['KEM'])
+    print(f'===== Multiplier: {multiplier} =====')
+    for key, value in items.items():
+        items[key] = {
+            **items[key],
+            'PPD': calculate_donut_price(multiplier=multiplier, **items[key]),
+            'DPI': donuts_per_item(multiplier=multiplier, **items[key])
+        }
+        print(f'{key.ljust(11)} - price-per-donut: {items[key]["PPD"]:2.2f}')
+        print(f'{key.ljust(11)} - donuts-per-item: {items[key]["DPI"]:2.2f}')
 
-    print(f'Multiplier: {multiplier}')
-    print(f'Price-Per-Donut Bloodmobile farming:: {price_for_bloodmobiles}')
-    print(f'Donuts per bloodmobile: {donuts_per_item(multiplier=multiplier, **items["Bloodmobile"])}')
-    print(f'Price-Per-Donut: KEM farming: {price_for_kem}')
-    print(f'Donuts per KEM: {donuts_per_item(multiplier=multiplier, **items["KEM"])}')
-    percentage = 100 - int(price_for_kem / price_for_bloodmobiles * 100)
-    print(f'KEM is %{int(percentage)} more cost efficient')
+    percentage = 100 - int(items['Kwik-E-Mart']['PPD'] / items['Bloodmobile']['PPD'] * 100)
+    print(f'KEM is %{int(percentage)} more cost efficient (Using MAX price only)')
 
 
 if __name__ == '__main__':
